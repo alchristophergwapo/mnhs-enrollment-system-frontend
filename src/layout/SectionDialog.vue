da9319d7c36b14a2085b5f51e684ce18a955c60c
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-04-08 13:09:43 +0800

    Updates 04/08

--- src/layout/SectionDialog.vue
+++ src/layout/SectionDialog.vue
@@ -65,7 +65,7 @@
                 :gradelevel="Number(type.split(' ')[2])"
                 :edit="type.split(' ')[0] == 'Edit' ? true : false"
                 :teacher="Section.teacher_name"
-                :modelValue="Section.teacher"
+                :modelValue="Section.teacher_name"
                 property="teacher_name"
               >
                 <template v-slot:label>
@@ -80,7 +80,8 @@
     <div>
       <v-card-title class="text-center justify-center py-6">
         <h1 class="font-weight-bold display-1 basil--text">
-          Class schedule (Optional)
+          Class schedule
+          <span v-if="type.split(' ')[0] != 'Edit'">(Optional)</span>
         </h1>
       </v-card-title>
       <v-form ref="scheduleForm" v-model="scheduleValid" lazy-validation>
@@ -343,14 +344,6 @@
         </div>
       </v-form>
     </div>
-
-    <!-- Dialog -->
-    <!-- <v-dialog v-model="editSchedule" persistent max-width="700px">
-      <edit-schedule-form
-        :schedules="schedToEdit"
-        :section_id="Section.id"
-      ></edit-schedule-form>
-    </v-dialog> -->
   </v-card>
 </template>
 <script>
@@ -397,6 +390,7 @@ export default {
         Friday: null,
       },
       schedToEdit: null,
+      sectionToEdit: this.Section,
       schedulesToAdd: [],
       teachers: [],
       errors: {},
@@ -423,6 +417,7 @@ export default {
   },
   watch: {},
   created() {
+    console.log(this.sectionToEdit);
     EventBus.$on("allSubjectsInGradeLevel", (data) => {
       console.log(data);
       let startTime =
@@ -460,6 +455,10 @@ export default {
       this.Section.teacher = data.data.id;
       console.log(this.Section);
     });
+
+    EventBus.$on('editallTeacher', (data) =>{
+      console.log(data.data);
+    })
   },
   mounted() {
     if (this.Section.id) {
