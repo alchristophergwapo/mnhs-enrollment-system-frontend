f2a46e7558dff4e2a0fdfae8f931147de282d5e9
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-04-21 19:57:19 +0800

    Updates 04/21

--- src/components/studentpage/StudentDetails.vue
+++ src/components/studentpage/StudentDetails.vue
@@ -37,7 +37,7 @@
 export default {
   props: ["student_details"],
   data() {
-    console.log("stud:", this.student_details);
+    console.log("stud:", this.student_details.section.name);
 
     return {
       student: this.student_details,
