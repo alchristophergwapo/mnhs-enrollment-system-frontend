cc215cfa0111ab303edc39d01023d83a11ab6ad8
MissQAssociates <msqintern0@gmail.com>
2021-05-09 10:25:28 +0800

    first-commitbyJJ

--- src/components/AllStudents.vue
+++ src/components/AllStudents.vue
@@ -522,7 +522,7 @@ export default {
     },
     //Method For Filtering By Grade Level
     filterByGradeLevel(grade) {
-      if (grade == "All") {
+      if (grade == "Alls") {
         this.selectGrade = "Allstudent";
         this.students = this.filteredStudents;
         let arraySection = [];
