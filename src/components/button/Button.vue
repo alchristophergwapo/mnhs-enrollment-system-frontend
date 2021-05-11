1abffb2dbc148111dd05b795fa51372d3c6af033
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-05-11 19:17:15 +0800

    Start normalizing components ang pages

---
+++ src/components/button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn>{{}}</v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    btnProperties: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
